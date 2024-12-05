6cf79d276580e0071108c03b4beb32693de4c6ae
sasindu399 <139614499+sasindu399@users.noreply.github.com>
2024-12-05 17:59:41 +0000

    database added

--- signup.php
+++ signup.php
@@ -1,3 +1,24 @@
+<?php
+  require_once 'connection/connection.php';
+
+  if(isset($_POST['signup'])){
+    $mail = mysqli_real_escape_string($connection,$_POST['mail']);
+    $fname = $_POST['fname'];
+
+    if($_POST['pwd']==$_POST['confpwd']){
+      $pwd = password_hash($_POST['pwd'],PASSWORD_DEFAULT);
+      $sql1 = "INSERT INTO Users (full_name, email, password_hash) VALUES('{$fname}','{$mail}', {'$pwd'})";
+      $result = mysqli_query($connection,$sql1);
+
+      if(isset($result)){
+        header("Location:login.php");
+      }
+
+    }
+
+
+  }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -48,24 +69,24 @@
     <div class="signup-container text-center">
         <img src="img/logo.png" alt="Top Left Image" style="width: 150px;" >
       <h3 class="mb-4" style="font-weight:700;">Create Your BuildTrack Account</h3>
-      <form>
+      <form action="signup.php" method="post">
         <div class="mb-3 text-start">
           <label for="fullName" class="form-label">Full Name</label>
-          <input type="text" class="form-control" id="fullName" placeholder="Enter your full name" required>
+          <input name="fname" type="text" class="form-control" id="fullName" placeholder="Enter your full name" required>
         </div>
         <div class="mb-3 text-start">
           <label for="email" class="form-label">Email Address</label>
-          <input type="email" class="form-control" id="email" placeholder="Enter your email" required>
+          <input name="mail" type="email" class="form-control" id="email" placeholder="Enter your email" required>
         </div>
         <div class="mb-3 text-start">
           <label for="password" class="form-label">Password</label>
-          <input type="password" class="form-control" id="password" placeholder="Create a password" required>
+          <input name="pwd" type="password" class="form-control" id="password" placeholder="Create a password" required>
         </div>
         <div class="mb-3 text-start">
           <label for="confirmPassword" class="form-label">Confirm Password</label>
-          <input type="password" class="form-control" id="confirmPassword" placeholder="Confirm your password" required>
+          <input name="confpwd"type="password" class="form-control" id="confirmPassword" placeholder="Confirm your password" required>
         </div>
-        <button type="submit" class="btn btn-primary w-100">Sign Up</button>
+        <button name="signup" type="submit" class="btn btn-primary w-100">Sign Up</button>
       </form>
       <div class="mt-3">
         <p>Already have an account? <a href="#">Login</a></p>
