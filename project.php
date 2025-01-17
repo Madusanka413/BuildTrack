29e9aa3100a47e3c007d7d47ec8e831324d98da0
Isuru Shehan Kodithuwakku <139618233+IsuruShehan99@users.noreply.github.com>
2025-01-17 06:05:39 +0000

    edit project

--- project.php
+++ project.php
@@ -8,7 +8,7 @@ $result = $connection->query($sql);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Project List</title>
+    <title>Our Project List</title>
     <style>
         body {
             font-family: Arial, sans-serif;
